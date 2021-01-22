90bf6bdf675ddbd279238a80488e0c0ff11b414d
dhilipsiva <dhilipsiva@pm.me>
2021-01-22 12:43:57 +0530

    Update Hello script

--- KonfHubDemo1/__init__.py
+++ KonfHubDemo1/__init__.py
@@ -5,7 +5,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
     name = req.params.get('name')
     if not name:
         try:
@@ -14,11 +13,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    name = name or 'Stranger'
+    return func.HttpResponse(f"Hello there, {name}! Welcome to KonfHubDemo!")
